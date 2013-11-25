c9c00caf9fe8c5d48616f28d5512b5fe19340cef
Toni Fadjukoff <lamperi@iki.fi>
2013-11-25 23:05:52 +0200

    Wonder trade and trade show implemented

--- test/pcapturer-test.js
+++ test/pcapturer-test.js
@@ -9,12 +9,35 @@ describe('PCapturer', function() {
     config.get.withArgs('filter').returns('')
 
     var pCapturer = new PCapturer(config)
+    afterEach(function() {
+        pCapturer.removeAllListeners()
+    })
 
     it('should emit "pokemon" for a good packet', function(once) {
         var packet = fs.readFileSync('test/raw-series/packet176.bin')
 
         pCapturer.on('pokemon', function(pokemon) {
             assert.equal("Gible", pokemon.name)
+            once()
+        })
+        pCapturer.networkCapturer.emit('udp_packet', packet)
+    })
+
+    it('should emit "pokemon" for a good packet, take 2', function(once) {
+        var packet = fs.readFileSync('data/udp-packet17.bin')
+
+        pCapturer.on('pokemon', function(pokemon) {
+            assert.equal("Gible", pokemon.name)
+            once()
+        })
+        pCapturer.networkCapturer.emit('udp_packet', packet)
+    })
+
+    it('should emit "pokemon" for a good packet, take 3', function(once) {
+        var packet = fs.readFileSync('data/udp-packet18.bin')
+
+        pCapturer.on('pokemon', function(pokemon) {
+            assert.equal("Froakie", pokemon.name)
             once()
         })
         pCapturer.networkCapturer.emit('udp_packet', packet)
